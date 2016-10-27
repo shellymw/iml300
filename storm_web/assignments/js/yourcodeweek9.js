fb52c844f5437f96ce71db469972ff57ac51ac9c
shellymw <michelle.whang1@yahoo.com>
2016-10-27 09:39:41 -0700

    week9

---
+++ storm_web/assignments/js/yourcodeweek9.js
@@ -0,0 +1,19 @@
+//$("#funkygreen").on("click", function(){
+//    $(this).parent().toggleClass("background", "green");
+//});
+
+$("#Janine").on ("click", function(){
+    $(this).parent().toggleClass("clickedJanine");
+});
+
+$("#Ryan").click(function(){
+    $(this).parent().toggleClass("clickedRyan");
+});
+
+$("#Alyssa").click(function(){
+    $(this).parent().toggleClass("clickedAlyssa");
+});
+
+$("#Lily").click(function(){
+    $(this).parent().toggleClass("clickedLily");
+});
